5e1c0c95a3fda3c4fa7a718b3a3483cf4fa81be9
Sofía Peñaherrera <e20212@ism.edu.ec>
2026-04-23 13:36:46 -0500

    Creación del archivo actividad 5

--- archivonuevoVScode.py
+++ archivonuevoVScode.py
@@ -1,3 +1,4 @@
+# TODOS LOS MÉTODOS DE LOS STRINGS
 print(nombre.find("f"))  #Devuelve la posición del primer carácter "f" en el string, si no lo encuentra devuelve -1
 print(nombre.replace("a", "o"))  #Reemplaza todas las apariciones de "a" por "o" en el string
 print(nombre.split("a"))  #Divide el string en una lista usando "a" como separador
